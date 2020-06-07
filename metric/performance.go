7030ed1104d0d06b4e38d02c8ed29b1da96f3a12
Chen Chen <ultraseven8937@gmail.com>
2020-06-06 19:15:39 -0700

    Add fault tolerance test (#107)
    * fault tolerance test

--- metric/performance.go
+++ metric/performance.go
@@ -27,6 +27,9 @@ const (
 	setCSV           = "metric/metric-set-%d.csv"
 	txnCSV           = "metric/metric-txn-%d-%s.csv"
 	readOnlyCSV      = "metric/metric-read-%d-%s.csv"
+	addCSV           = "metric/metric-add-%d.csv"
+	xferCSV          = "metric/metric-xfer.csv"
+	faultTolerance   = "metric/metric-fault-tol.csv"
 )
 
 var (
@@ -229,6 +232,179 @@ func TestTxnLatency(filePath string, conflictRate int, singleShard, readOnly boo
 	}
 }
 
+func TestAddLatency(filePath string, conflictRate int) {
+	file, err := os.Create(fmt.Sprintf(filePath, conflictRate))
+	checkError("Cannot create file", err)
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for idx, numClient := range clientsToTest {
+		title := fmt.Sprintf("client%d", numClient)
+		log.Println(title)
+		latencyRow := []string{title}
+		latencies := make([][]int, numClient)
+		var clients []*client.RaftKVClient
+		for i := 0; i < numClient; i++ {
+			c := client.NewRaftKVClient(coordAddr, clientTimeout)
+			clients = append(clients, c)
+			c.Set(strconv.Itoa(i), int64(i))
+		}
+		var wg sync.WaitGroup
+		for i, c := range clients {
+			wg.Add(1)
+			go func(c *client.RaftKVClient, k int) {
+				defer wg.Done()
+				expStart := time.Now()
+				for time.Since(expStart) < requestDuration {
+					var key string
+					if conflictRate == 0 {
+						key = strconv.Itoa(k)
+					} else {
+						key = fmt.Sprint(intToHash(k) % (numClient/conflictRate + 1))
+					}
+					start := time.Now()
+					err := c.AddTransaction([]string{common.ADD, key, "1"})
+					latencies[k] = append(latencies[k], int(time.Since(start)/time.Microsecond))
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			}(c, i)
+		}
+		wg.Wait()
+		for _, lat := range latencies {
+			for _, l := range lat {
+				latencyRow = append(latencyRow, strconv.Itoa(l))
+			}
+		}
+		err := writer.Write(latencyRow)
+		checkError("Cannot write to file", err)
+		coolDown(clientsToTest, idx)
+	}
+}
+
+func TestXferLatency(filePath string) {
+	file, err := os.Create(filePath)
+	checkError("Cannot create file", err)
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for idx, numClient := range clientsToTest {
+		offset := 100000
+		title := fmt.Sprintf("client%d", numClient)
+		log.Println(title)
+		latencyRow := []string{title}
+		latencies := make([][]int, numClient)
+		var clients []*client.RaftKVClient
+		for i := 0; i < numClient; i++ {
+			c := client.NewRaftKVClient(coordAddr, clientTimeout)
+			clients = append(clients, c)
+			c.Set(strconv.Itoa(offset+i), int64(offset+i))
+			c.Set(strconv.Itoa(i), int64(i))
+		}
+		var wg sync.WaitGroup
+		for i, c := range clients {
+			wg.Add(1)
+			go func(c *client.RaftKVClient, k int) {
+				defer wg.Done()
+				expStart := time.Now()
+				for time.Since(expStart) < requestDuration {
+					start := time.Now()
+					err := c.TransferTransaction([]string{common.TRANSFER, strconv.Itoa(offset + k), strconv.Itoa(k), "1"})
+					latencies[k] = append(latencies[k], int(time.Since(start)/time.Microsecond))
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			}(c, i)
+		}
+		wg.Wait()
+		for _, lat := range latencies {
+			for _, l := range lat {
+				latencyRow = append(latencyRow, strconv.Itoa(l))
+			}
+		}
+		err := writer.Write(latencyRow)
+		checkError("Cannot write to file", err)
+		coolDown(clientsToTest, idx)
+	}
+}
+
+type timeStamp struct {
+	start   int
+	end     int
+	err     string
+	op      string
+}
+
+func TestFaultTolerance(filePath string, numClient int, getPercent float32) {
+	file, err := os.Create(filePath)
+	checkError("Cannot create file", err)
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	timestampCol := []string{"start", "end", "error", "op"}
+	err = writer.Write(timestampCol)
+	checkError("Cannot write to file", err)
+	timestamps := make([][]timeStamp, numClient)
+
+	var clients []*client.RaftKVClient
+	for i := 0; i < numClient; i++ {
+		c := client.NewRaftKVClient(coordAddr, clientTimeout)
+		clients = append(clients, c)
+	}
+	var wg sync.WaitGroup
+	for i, c := range clients {
+		wg.Add(1)
+		go func(c *client.RaftKVClient, k int) {
+			defer wg.Done()
+			expStart := time.Now()
+			key := strconv.Itoa(k)
+			for time.Since(expStart) <60*time.Second {
+				var start, end time.Duration
+				//var success bool
+				var op, errStr string
+				if float32(k)/float32(numClient) < getPercent {
+					start = time.Since(expStart)
+					err := c.Get(key)
+					end = time.Since(expStart)
+					if err != nil && err.Error() != fmt.Sprintf("Key=%s does not exist", key) {
+						errStr = err.Error()
+					}
+					op = common.GET
+				} else {
+					start = time.Since(expStart)
+					err := c.Set(key, int64(k))
+					end = time.Since(expStart)
+					if err != nil {
+						errStr = err.Error()
+					}
+					op = common.SET
+				}
+				timestamps[k] = append(
+					timestamps[k],
+					timeStamp{
+						start:   int(start) / int(time.Nanosecond),
+						end:     int(end) / int(time.Nanosecond),
+						err: errStr,
+						op:      op,
+					},
+				)
+			}
+		}(c, i)
+	}
+	wg.Wait()
+	for _, tt := range timestamps {
+		for _, t := range tt {
+			timestampCol = []string{strconv.Itoa(t.start), strconv.Itoa(t.end), t.err, t.op}
+			err := writer.Write(timestampCol)
+			checkError("Cannot write to file", err)
+		}
+	}
+}
+
 func coolDown(clientsToTest []int, idx int) {
 	if idx != len(clientsToTest)-1 {
 		fmt.Printf("Waiting %s to cool down for %d clients test...\n", coolDownDuration, clientsToTest[idx+1])
@@ -257,4 +433,9 @@ func main() {
 	}
 	TestTxnLatency(txnCSV, 0, false, false)
 	TestTxnLatency(readOnlyCSV, 0, false, true)
+	for _, i := range []int{0, 2, 5, 10} {
+		TestAddLatency(addCSV, i)
+	}
+	TestXferLatency(xferCSV)
+	TestFaultTolerance(faultTolerance, 5, 0.8)
 }
